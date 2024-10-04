63fbf0e69d955cf62c993125360eca233ebe55b1
saulltech <saulkumven@gmail.com>
2024-10-04 13:56:36 +0100

    added react and tailwind template

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './index.css'
+
+
+function App() {
+  return (
+    <div className="bg-black text-white border-l-8">
+      <div className='bg-black'>
+        helllo css world good
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
