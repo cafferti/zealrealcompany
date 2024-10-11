a871a029fc526aac2288a3f5030d92b20b962d33
saulltech <saulkumven@gmail.com>
2024-10-11 11:03:58 +0100

    working on footer

--- src/landing.tsx
+++ src/landing.tsx
@@ -9,7 +9,8 @@ import saulltecharrow from './assets/Arrow 1.svg'
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import saulltecharrowback from './assets/Frame 155 (1).svg'
 import saulltecharrowfront from './assets/frameack.svg'
-import { faBed, faBath, faStar } from '@fortawesome/free-solid-svg-icons';
+import { faBed, faBath, faStar, } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-regular-svg-icons/faHeart'
 
@@ -679,8 +680,8 @@ function Nav() {
         </div>
       </div>
 
-      <div className=" px-[10%] text-center pt-14 bg-[#f4f4f4] sm:px-60 md:px-[30%]">
-        <h1 className='tracking-tight text-2xl py-2 font-serif pb-6' >What Our Customers Think</h1>
+      <div className=" px-[10%] text-center pt-14 bg-[#f4f4f4] sm:px-60 md:px-[30%] h-fit">
+        <h1 className='tracking-tight text-2xl py-2 font-serif pb-6'>What Our Customers Think</h1>
         <Swiper
           // install Swiper modules
           modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]} // Add Autoplay module
@@ -697,21 +698,15 @@ function Nav() {
         >
           <SwiperSlide>
             <h2 className='tracking-tight leading-tight text-lg pt-6 font-[17px]'>
-
-              {/* "The Property Webmasters team are a joy to work with, they are always friendly, understanding but, most importantly, very strategically astute. PWM has helped FrenchEntrée develop our French Property Database and lead generation strategy and we are incredibly happy with the level of performance. I would recommend Property Webmasters to everyone, however, rather selfishly I want to keep the PWM team to us." */}
-
-              {/* Ben Stephens, Frenchentree */}
-              {/* "Property Webmasters have built and designed our estate agency website. They have a ‘can-do’ attitude and nothing is a problem. I highly recommend them." */}
-
-              "For the 8 years I have worked with PW I have been consistently impressed with their innovative ideas and dedication. They have helped us nurture our business from start-up to established island portal and provided us with industry insights to both maximise our growth and onboard new technology. The team is always reliable and on time, often working out of hours to complete tasks. They have also assisted greatly with our marketing, SEO, new product launch and new idea generation. I would not hesitate to recommend the team to help your business grow and flourish."</h2>
+              "For the 8 years I have worked with PW I have been consistently impressed with their innovative ideas and dedication. They have helped us nurture our business from start-up to established island portal and provided us with industry insights to both maximise our growth and onboard new technology. The team is always reliable and on time, often working out of hours to complete tasks. They have also assisted greatly with our marketing, SEO, new product launch and new idea generation. I would not hesitate to recommend the team to help your business grow and flourish."
+            </h2>
             <p className='py-2 text-[#226666] tracking-tight text-sm'>
               Richard Shelton, Shelton and Lines
             </p>
           </SwiperSlide>
-40
-          <SwiperSlide>
 
-            <h2 className='tracking-tight leading-tight text-lg pt-6 font-[17px]' >
+          <SwiperSlide>
+            <h2 className='tracking-tight leading-tight text-lg pt-6 font-[17px]'>
               "What an experience. I enquired about online marketing for my property business in Spain in the middle of the Covid, and within 20 days we had the ads set up, landing pages created, and leads flooding in from the first day! To put the cherry on the cake, we had our first conversion after only two weeks of advertising, making us over 1950% ROI!!! Thank you Luke, as you said, we’re just getting started."
             </h2>
             <p className='py-2 text-[#226666] tracking-tight text-sm'>
@@ -720,30 +715,25 @@ function Nav() {
           </SwiperSlide>
 
           <SwiperSlide>
-
-            <h2 className='tracking-tight leading-tight text-lg pt-6 font-[17px]' >
+            <h2 className='tracking-tight leading-tight text-lg pt-6 font-[17px]'>
               "What an experience. I enquired about online marketing for my property business in Spain in the middle of the Covid, and within 20 days we had the ads set up, landing pages created, and leads flooding in from the first day! To put the cherry on the cake, we had our first conversion after only two weeks of advertising, making us over 1950% ROI!!! Thank you Luke, as you said, we’re just getting started."
             </h2>
             <p className='py-2 text-[#226666] tracking-tight text-sm'>
               Oliver Hornby, One Property Group
             </p>
           </SwiperSlide>
-
 
           <SwiperSlide>
-
-            <h2 className='tracking-tight leading-tight text-lg pt-6 font-[17px]' >
+            <h2 className='tracking-tight leading-tight text-lg pt-6 font-[17px]'>
               "What an experience. I enquired about online marketing for my property business in Spain in the middle of the Covid, and within 20 days we had the ads set up, landing pages created, and leads flooding in from the first day! To put the cherry on the cake, we had our first conversion after only two weeks of advertising, making us over 1950% ROI!!! Thank you Luke, as you said, we’re just getting started."
             </h2>
             <p className='py-2 text-[#226666] tracking-tight text-sm'>
               Oliver Hornby, One Property Group
             </p>
           </SwiperSlide>
-
 
           <SwiperSlide>
-
-            <h2 className='tracking-tight leading-tight text-lg pt-6 font-[17px]' >
+            <h2 className='tracking-tight leading-tight text-lg pt-6 font-[17px]'>
               "What an experience. I enquired about online marketing for my property business in Spain in the middle of the Covid, and within 20 days we had the ads set up, landing pages created, and leads flooding in from the first day! To put the cherry on the cake, we had our first conversion after only two weeks of advertising, making us over 1950% ROI!!! Thank you Luke, as you said, we’re just getting started."
             </h2>
             <p className='py-2 text-[#226666] tracking-tight text-sm'>
@@ -751,13 +741,88 @@ function Nav() {
             </p>
           </SwiperSlide>
 
-
-
-          ...
         </Swiper>
-
-
       </div>
+
+      <footer className=' border border-t border-gray-300 mt-6'>
+        <h1 className='py-8 px-8 text-center text-[17px] font-forumSerif text-2xl pb-8 cursor-pointer' >Popular Location</h1>
+        <section className='bg-[#0d4d4d] text-center py-12 text-white sm:flex'>
+          <h1 className='leading-tight text-2xl  mb-1 font-forumSerif'> Sign up to our  newsletter</h1>
+          <h2 className='tracking-tight leading-tight pb-4 mb-4  font-[17px]'> stay up to date with latest news</h2>
+          <div>
+          <input className=' w-[90%] rounded-lg text-sm px-4 border-2 h-11 mb-4 border-gray-50 ' type="text" placeholder='Fullname' />
+          <div className=' justify-between items-center  w-[90%] text bg-white mx-auto px-4 pr-1 border-2 h-11 mb-4 border-gray-50 rounded-lg'>
+            <input className='w-[90%] mx-auto' type="text" placeholder='Enter Email' />
+            <button className='bg-[#aa3939] rounded-lg transition-all tracking-wide uppercase font-bold text-sm px-4 h-9'>
+              signup
+            </button>
+          </div>
+
+          </div>
+        </section>
+        <div className='bg-[#003333] md:text-left text-center py-5 md:grid lg:grid-cols-5 sm:grid-cols-4'>
+          <img className='w-[50%] mx-auto md:hidden' src='https://agentplus-s3.s3.eu-west-2.amazonaws.com/discover-agentplus/logos/2022/08/20/0d604b5a-79f0-4f94-91de-96ba4aab2d7e.png' alt="" />
+
+          <div className='text-white py-6 border-b md:border-none  sm:mx-[10%]'>
+            <h1 className='tracking-tight font-bold text-lg '>Menu</h1>
+            <div className='mt-3 '>
+              <a className='text-[17px] opacity-90 block' href="">Buy properties</a>
+              <a className='text-[17px] opacity-90 block' href=""> About Us</a>
+              <a className='text-[17px] opacity-90 block' href="">Meet The Team</a>
+              <a className='text-[17px] opacity-90 block' href="">Testimonial</a>
+              <a className='text-[17px] opacity-90 block' href="">News</a>
+              <a className='text-[17px] opacity-90 block' href="">Contaact Us</a>
+            </div>
+          </div>
+
+          <div className='text-white py-6 border-b md:border-none  sm:mx-[10%]'>
+            <h1 className='tracking-tight font-bold text-lg'>Socials</h1>
+            <div className='mt-3'>
+              <a className='text-[17px] opacity-90 block' href="">Facebook</a>
+              <a className='text-[17px] opacity-90 block' href=""> LinkedIn</a>
+              <a className='text-[17px] opacity-90 block' href="">Instagram</a>
+
+            </div>
+          </div>
+
+
+          <div className='text-white py-6 border-b md:border-none  sm:mx-[10%]'>
+            <h1 className='tracking-tight font-bold text-lg'>Company</h1>
+            <div className='mt-3'>
+              <a className='text-[17px] opacity-90 block' href="">Privacy Policy</a>
+              <a className='text-[17px] opacity-90 block' href=""> Terms and Condition</a>
+            </div>
+          </div>
+
+          <div className='text-white py-6 border-b md:border-none  sm:mx-[10%]'>
+            <h1 className='tracking-tight font-bold text-lg'>Get In Touch</h1>
+            <div className='mt-3'>
+              <a className='text-[17px] opacity-90 block' href="">+44 (777 444 555)</a>
+              <a className='text-[17px] opacity-90 block' href=""> saulltech@gmail.com</a>
+            </div>
+          </div>
+
+          <div className='text-white py-6   sm:mx-[10%]'>
+            <h1 className='tracking-tight font-bold text-lg'>Resources</h1>
+            <div className='mt-3'>
+              <a className='text-[17px] opacity-90 block' href="">Download Our  Free Guide</a>
+              <a className='text-[17px] opacity-90 block' href=""> Area Guide</a>
+            </div>
+          </div>
+
+        </div>
+        <div className='py-5 bg-[#003333] text-center border-t'>
+            <FontAwesomeIcon icon={faLinkedin} className="text-xl px-2 text-white opacity-90" />
+            <FontAwesomeIcon icon={faFacebook} className="text-xl px-2 text-white opacity-90" />
+            <FontAwesomeIcon icon={faInstagram} className="text-xl px-2 text-white opacity-90" />
+            <FontAwesomeIcon icon={faTwitter} className="text-xl px-2 text-white opacity-90" />
+            <p className='text-white pt-7 opacity-70'>
+              Copyright Discover 2024. All rights reserved.
+            </p>
+          </div>
+
+
+      </footer>
 
 
 
